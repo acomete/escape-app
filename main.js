b2adb1efe17f458b9a8841b9304589fb1e96045c
Pierre-Gérard David <pierre-gerard.david@doc.fr>
2023-03-14 00:28:02 +0100

    Lanzarote.

--- main.js
+++ main.js
@@ -3,83 +3,90 @@ const path = require('path')
 const Mousetrap = require('mousetrap');
 const usbDetect = require('usb-detection');
 
-const serialNumber = '00015214122921193202'
+const serialNumber = '0719971F1AA12627'
 
 let win
 
 const createWindow = () => {
-    win = new BrowserWindow({
-        width: 1000,
-        height: 500,
-        frame: false,
-        fullscreen: true,
-        alwaysOnTop: true,
-        closable: false,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+  win = new BrowserWindow({
+    width: 1000,
+    height: 500,
+    frame: false,
+    fullscreen: true,
+    alwaysOnTop: true,
+    closable: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  usbDetect.find()
+      .then((devices) => {
+        detected = devices.find(device => device.serialNumber === serialNumber)
+
+        if (!detected) {
+          win.loadFile('views/auth.html')
+        } else {
+          win.loadFile('views/restarted.html')
         }
-    })
+      })
 
-    usbDetect.find()
-        .then((devices) => {
-            detected = devices.find(device => device.serialNumber === serialNumber)
+  win.webContents.on("before-input-event", (event, input) => {
+    console.log(input)
+    if (input.code === 'F4' && input.alt)
+      event.preventDefault();
+  });
 
-            if (!detected) {
-                win.loadFile('views/auth.html')
-            } else {
-                win.loadFile('views/restarting.html')
-            }
-        })
+  /*Mousetrap.bind(['command+k', 'ctrl+k'], () => {
+    console.log('quit')
+    app.quit()
+  });*/
 
-    window.webContents.on("before-input-event", (event, input) => {
-        console.log(input)
-                if (input.code === 'F4' && input.alt)
-                    event.preventDefault();
-            });
-
-    Mousetrap.bind(['command+k', 'ctrl+k'], () => {
-        console.log('quit')
-        app.quit()
-    });
-
-    win.maximize()
+  win.maximize()
 }
 
 const menu = new Menu()
 menu.append(new MenuItem({
-    label: 'Electron',
-    submenu: [{
-        role: 'help',
-        accelerator: process.platform === 'Alt+F4',
-        click: () => { console.log('Electron rocks!') }
-    }]
+  label: 'Electron',
+  submenu: [{
+    role: 'help',
+    accelerator: process.platform === 'Alt+F4',
+    click: () => {
+      console.log('Electron rocks!')
+    }
+  }]
 }))
 
 Menu.setApplicationMenu(menu)
 
 app.whenReady().then(() => {
-    createWindow()
-    usbDetect.startMonitoring();
+  createWindow()
+  usbDetect.startMonitoring();
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 
-    usbDetect.on('add', (device) => {
-        console.log(device);
-        if (device.serialNumber === serialNumber) {
-            win.loadFile('views/restarting.html')
-        }
-    });
+  usbDetect.on('add', (device) => {
+    console.log(device);
 
-    usbDetect.on('remove', (device) => {
-        console.log(device);
-        if (device.serialNumber === serialNumber) {
-            win.loadFile('views/auth.html')
-        }
-    });
+    if (device.serialNumber === serialNumber) {
+      win.loadFile('views/restarting.html')
+
+      setTimeout(() => {
+        win.loadFile('views/restarted.html', {"extraHeaders" : "pragma: no-cache\n"})
+      }, 3000)
+    }
+  });
+
+  usbDetect.on('remove', (device) => {
+    console.log(device);
+    if (device.serialNumber === serialNumber) {
+      win.loadFile('views/auth.html')
+    }
+  });
 })
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') app.quit()
 })
